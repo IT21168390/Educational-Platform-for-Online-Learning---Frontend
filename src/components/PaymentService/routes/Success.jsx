8134405f5ff28c6ec8e8de8519a895c8dccea7f8
Saranga Gunasekara <it21168222@my.sliit.lk>
2024-05-13 15:36:42 +0530

    update React

--- src/components/PaymentService/routes/Success.jsx
+++ src/components/PaymentService/routes/Success.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Button, Center, Heading, Text, VStack } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
