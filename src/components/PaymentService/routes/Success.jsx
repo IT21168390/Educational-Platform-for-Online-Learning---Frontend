f8ad69dcedb8bcc442061b268cd5e0e66786ca7d
Saranga Gunasekara <it21168222@my.sliit.lk>
2024-05-13 14:32:49 +0530

    update success

--- src/components/PaymentService/routes/Success.jsx
+++ src/components/PaymentService/routes/Success.jsx
@@ -3,12 +3,39 @@ import { Button, Center, Heading, Text, VStack } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 function Success() {
+    // const [enrolledCourseId, setEnrolledCourseId] = useState("");
+    // const [userId, setUserId] = useState("");
     const queryParams = new URLSearchParams(window.location.search);
     const navigate = useNavigate();
 
     const onButtonClick = () => {
         navigate("/");
     };
+
+    // useEffect(() => {
+    //     const enrollUser = async () => {
+    //         try {
+    //             const data = {
+    //                 userEnrolledCourseDto: {
+    //                     enrolledCourseId: enrolledCourseId,
+    //                     userId: userId
+    //                 }
+    //             };
+    //             await axios.post("http://localhost:8081/api/LearnerService/enrollCourse", data);
+    //             // Redirect to viewMyCourses after successful enrollment
+    //             window.location.href = '/viewMyCourses';
+    //         } catch (error) {
+    //             console.error("Error occurred while enrolling:", error);
+    //         }
+    //     };
+        
+    //     enrollUser();
+
+    //     // Cleanup function (if needed)
+    //     return () => {
+    //         // Any cleanup code here
+    //     };
+    // }, []);
 
     return (
         <Center h="100vh" color="green">
